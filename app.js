bb1636d40d53f95d4b0990365a8f3a5940e2bfc3
StefanTheSpider <stefantuaevgmail.com>
2023-05-05 14:57:51 +0200

    pot request add

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
@@ -12,7 +13,29 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(reg, res) {
-  res.render('index.ejs', {mainContant: homeStartingContent});
+  res.render('index.ejs', {mainContent: homeStartingContent});
+});
+
+app.get('/about', function(req,res) {
+  res.render('about.ejs', {aboutContent: aboutContent});
+});
+
+app.get('/contact', function(req,res) {
+  res.render('contact.ejs', {contactContent: contactContent});
+});
+
+app.get('/compose', function(req,res) {
+  res.render('compose.ejs', {newBlogTitle: newBlogTitle, newBlogPost: newBlogPost});
+});
+
+let newBlogTitle = [];
+let newBlogPost = [];
+app.post('/compose', function(req, res) {
+  let title = req.body.blogPostTitle;
+  let post = req.body.blogPostPost
+  newBlogPost.push(post)
+  newBlogTitle.push(title);
+  res.redirect('/compose');
 });
 
 app.listen(3000, () => console.log('Server is runing, blog is redy'));
